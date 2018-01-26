2194bd559ebf5913245530b5916a4606961dbbf6
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-26 07:36:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,5 +4,20 @@ function init() {
   // Write your JavaScript code inside the init() function
   var index = 0;
   
-  document
+  document.body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.which);
+    
+    if(key === code[index]){
+      index++;
+      
+      if(index === code.length){
+        alert('IT IS DONE.');
+        index = 0;
+      }
+    }
+    else{
+      index = 0;  
+    }
+    
+  });
 }
